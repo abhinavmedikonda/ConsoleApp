71765a69971b3c603fd1ab1dde3a9d530e647d43
Abhinav Medikonda <v-abmedi@microsoft.com>
2018-04-11 22:55:44 -0700

    CheckIfProgressed

--- ConsoleApp/Cache.cs
+++ ConsoleApp/Cache.cs
@@ -16,21 +16,11 @@ namespace ConsoleApp
             {
                 itemsIndex.TryGetValue(theitem, out int index);
                 mvFrequency[index] = mvFrequency[index] + 1;
-                if (index > 0 && mvFrequency[index] >= mvFrequency[index - 1])
-                {
-                    int frequency = mvFrequency[index];
-                    string item = mvItem[index];
-                    mvFrequency[index] = mvFrequency[index - 1];
-                    mvItem[index] = mvItem[index - 1];
-                    mvFrequency[index - 1] = frequency;
-                    mvItem[index - 1] = item;
-                    itemsIndex[mvItem[index]] = index;
-                    itemsIndex[mvItem[index - 1]] = index - 1;
-                }
+                CheckIfProgressed(index);
             }
             else
             {
-                if(itemsIndex.Count < 100)
+                if (itemsIndex.Count < 100)
                 {
                     mvFrequency[itemsIndex.Count] = 1;
                     mvItem[itemsIndex.Count] = theitem;
@@ -42,6 +32,22 @@ namespace ConsoleApp
                     mvItem[itemsIndex.Count - 1] = theitem;
                     itemsIndex.Add(theitem, itemsIndex.Count - 1);
                 }
+            }
+        }
+
+        private void CheckIfProgressed(int index)
+        {
+            if (index > 0 && mvFrequency[index] >= mvFrequency[index - 1])
+            {
+                int frequency = mvFrequency[index];
+                string item = mvItem[index];
+                mvFrequency[index] = mvFrequency[index - 1];
+                mvItem[index] = mvItem[index - 1];
+                mvFrequency[index - 1] = frequency;
+                mvItem[index - 1] = item;
+                itemsIndex[mvItem[index]] = index;
+                itemsIndex[mvItem[index - 1]] = index - 1;
+                CheckIfProgressed(index - 1);
             }
         }
     }
